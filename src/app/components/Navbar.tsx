d507d18cb012a1e704d573220e4846f7466fe6c6
Bárbara Dias <rm556974@fiap.com.br>
2025-04-01 20:48:18 -0300

    logo

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <header className="bg-[#2D8C44] h-20 flex justify-between items-center px-6 text-white font-bold">
-        <img src="/public/ChatGPT Image 1_04_2025, 20_37_53.png" alt="" />
+        <img src="/public/ChatGPT Image 1_04_2025, 20_46_27.png" alt="" />
         <nav>
           <ul className="flex gap-5">
             <li><Link to="/">Home</Link></li>
